84da064556cd329ab48225aa02be95c6c6da7c52
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2022-09-26 08:21:08 +0200

    improvements to map downloader

--- scripts/download_map_folder.sh
+++ scripts/download_map_folder.sh
@@ -10,11 +10,19 @@ files=$(curl -X GET $1 2>&1 | grep latest.osm.pbf | awk '{print $6}' | awk -F "\
 for file in $files
 do
 	area=$(basename $file -latest.osm.pbf)
+	cur_dir=$(pwd)
 	if test -f "$file";
 	then
-		echo -e "${RED}$area already exists, skipping..${RESET}"
+		if test -f "$area.mbtiles";
+		then
+			echo -e "${RED}$area already exists as .mbtiles, skipping..${RESET}"
+		else
+			echo -e "${PURPLE} converting $file to .mbtiles${RESET}"
+			(cd ~/git/tilemaker/ && tilemaker --input "$cur_dir/$file" --output "$pwd/$area.mbtiles")      
+		fi
 	else
 		echo -e "${PURPLE}Downloading map data for ${GREEN}$area${RESET}"
 		curl -O -X GET "$1$file"
+		(cd ~/git/tilemaker/ && tilemaker --input "$cur_dir/$file" --output "$pwd/$area.mbtiles")      
 	fi
 done
