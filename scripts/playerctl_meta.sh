e8ca7461f90caa76f21a93000d697e95e5b54a8b
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2022-11-27 12:36:02 +0100

    feat(music): Display music status in metadata

--- scripts/playerctl_meta.sh
+++ scripts/playerctl_meta.sh
@@ -3,9 +3,17 @@
 # Operate on any players, but prioritze cider
 PLAYER_PRIORITY="cider,%any"
 
+# Set status icon
+if [ $(playerctl --player=$PLAYER_PRIORITY status) == "Playing" ];
+then
+	STATUS_ICON="⏸"
+else
+	STATUS_ICON="⏵"
+fi
+
 if [[ -z $(playerctl -l) ]];
 then
 	echo "No players available!"
 else
-	echo "$(playerctl metadata --player=$PLAYER_PRIORITY --format "{{artist}} - {{title}}")"
+	echo "$(playerctl metadata --player=$PLAYER_PRIORITY --format "{{artist}} - {{title}}") $STATUS_ICON "
 fi
