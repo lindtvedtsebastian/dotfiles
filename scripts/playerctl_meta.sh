28a65f2dc273f4ec6c99615a287ed2496925184a
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2022-11-27 12:56:39 +0100

    feat(music): playerctl meta string formatting

--- scripts/playerctl_meta.sh
+++ scripts/playerctl_meta.sh
@@ -6,14 +6,14 @@ PLAYER_PRIORITY="cider,%any"
 # Set status icon
 if [ $(playerctl --player=$PLAYER_PRIORITY status) == "Playing" ];
 then
-	STATUS_ICON="⏸"
+	STATUS_ICON="⏸ "
 else
-	STATUS_ICON="⏵"
+	STATUS_ICON="⏵︎ "
 fi
 
 if [[ -z $(playerctl -l) ]];
 then
 	echo "No players available!"
 else
-	echo "$(playerctl metadata --player=$PLAYER_PRIORITY --format "{{artist}} - {{title}}") $STATUS_ICON "
+	echo "$(playerctl metadata --player=$PLAYER_PRIORITY --format "{{artist}} - {{title}}") $STATUS_ICON"
 fi
