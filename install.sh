38858c208333627fca35314e37a481311d577c9c
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2023-02-07 18:16:47 +0100

    feat(install): added gnupg.org to list of files to be tangled

--- install.sh
+++ install.sh
@@ -2,3 +2,4 @@
 source ~/dotfiles/scripts/tangle_org_file.sh emacs.org
 source ~/dotfiles/scripts/tangle_org_file.sh zsh.org
 source ~/dotfiles/scripts/tangle_org_file.sh xmonad.org
+source ~/dotfiles/scripts/tangle_org_file.sh gnupg.org
