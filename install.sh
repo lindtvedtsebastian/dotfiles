2ee4cacfef23cc68305164008d57afc4af39d08e
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2023-05-07 08:50:04 +0200

    feat(xorg): added xinit.org and updated install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ source ~/dotfiles/scripts/tangle_org_file.sh emacs.org
 source ~/dotfiles/scripts/tangle_org_file.sh zsh.org
 source ~/dotfiles/scripts/tangle_org_file.sh xmonad.org
 source ~/dotfiles/scripts/tangle_org_file.sh gnupg.org
+source ~/dotfiles/scripts/tangle_org_file.sh xinit.org 
