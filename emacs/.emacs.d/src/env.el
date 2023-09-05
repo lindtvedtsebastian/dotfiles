e0bbb0aa874ab37a95881decb6cea21218614436
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2023-09-05 18:54:24 +0200

    feat(emacs/env): env variable configurations

---
+++ emacs/.emacs.d/src/env.el
@@ -0,0 +1,10 @@
+;;; env.el --- -*- lexical-binding: t -*-
+;;; commentary:
+;;; env variable configurations
+;;; code:
+
+(require 'exec-path-from-shell)
+
+(exec-path-from-shell-initialize)
+
+;;; env.el ends here
