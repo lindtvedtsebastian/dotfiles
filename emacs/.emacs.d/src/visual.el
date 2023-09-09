5925173b9120e752ed672ca6dafd61fd02f674a7
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2023-09-09 08:24:11 +0200

    feat(emacs/visual): setup visuals in new config

---
+++ emacs/.emacs.d/src/visual.el
@@ -0,0 +1,10 @@
+;;; visual.el --- -*- lexical-binding: t -*-
+;;; commentary:
+;; visual Emacs configurations
+;;; code:
+
+(setq show-paren-delay 0) ; No delay before highlighting parenthesis
+
+(set-face-attribute 'default nil :font "Iosevka SS08" :weight 'normal :width 'normal :height 140)
+
+;;; visual.el ends here
