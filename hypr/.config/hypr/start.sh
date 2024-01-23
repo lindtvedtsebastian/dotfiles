fbbac43407c0c83eb4a73ae19e28b56d539d3d72
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2024-01-23 17:48:57 +0100

    feat(hypr/setup): added hyprland config

---
+++ hypr/.config/hypr/start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+1password --silent &
+
+swww init &
+
+nm-applet --indicator &
+
+waybar &
+
+dunst & 
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 stow --verbose=2 --ignore .DS_Store emacs/ 
 stow --verbose=2 --ignore .DS_Store zsh/
+stow --verbose=2 --ignore .DS_Store hypr/ 
 stow --verbose=2 --ignore .DS_Store waybar/ 
