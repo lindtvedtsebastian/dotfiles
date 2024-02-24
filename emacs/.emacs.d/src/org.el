7fd9e2e453d9aff41a544f2e902aa1a657292a30
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2024-02-24 09:30:10 +0100

    feat(emacs/org): simple org-roam setup with inbox capturing

--- emacs/.emacs.d/src/org.el
+++ emacs/.emacs.d/src/org.el
@@ -3,8 +3,23 @@
 ;; Emacs configurations for org mode
 ;;; code:
 
+(require 'org)
+(setq org-directory "~/st/org")
+
 (require 'org-roam)
 
-(setq org-roam-node-display-template "${title:*} ${tags:*}")
+(setq org-roam-directory "~/st/org/roam")
+(setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:30}" 'face 'org-tag)))
+
+(defun sl/org-roam-capture-inbox ()
+  "Capture something directly into the roam inbox."
+  (interactive)
+  (org-roam-capture- :node (org-roam-node-create)
+                     :templates '(("n" "note" plain "* %?"
+                                   :if-new (file+head "inbox.org" "#+title: inbox\n"))
+                                  ("t" "todo" plain "* TODO %?" :if-new (file+head "inbox.org" "#+title: inbox\n"))
+                                  )))
+
+(org-roam-db-autosync-mode)
 
 ;;; org.el ends here
