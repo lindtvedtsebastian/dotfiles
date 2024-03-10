464da7af080b72cb39af8d7c88bcab0ddc599c99
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2024-03-10 08:52:50 +0100

    feat(ags): setup flake.nix

---
+++ ags/.config/ags/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "ags";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default =
+        pkgs.mkShell
+          {
+            buildInputs = with pkgs; [
+              typescript
+              nodePackages.prettier
+              nodePackages.eslint
+              nodePackages.typescript-language-server
+            ];
+          };
+
+    };
+}
