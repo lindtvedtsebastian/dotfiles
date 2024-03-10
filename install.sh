e330ea31ae9e4f1c5ec23aead386650f427ca9c4
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2024-03-10 08:46:06 +0100

    feat(dots): added ags

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ stow --verbose=2 --ignore .DS_Store emacs/
 stow --verbose=2 --ignore .DS_Store zsh/
 stow --verbose=2 --ignore .DS_Store hypr/ 
 stow --verbose=2 --ignore .DS_Store waybar/ 
+stow --verbose=2 --ignore .DS_Store ags/ 
--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 stow --verbose=2 --ignore .DS_Store -D emacs/ 
 stow --verbose=2 --ignore .DS_Store -D zsh/
+stow --verbose=2 --ignore .DS_Store -D hypr/ 
+stow --verbose=2 --ignore .DS_Store -D waybar/ 
+stow --verbose=2 --ignore .DS_Store -D ags/ 
