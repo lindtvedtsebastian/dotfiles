f844baa151559f609d2f0b89c1d14c711baad84e
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2024-03-15 14:12:45 +0100

    feat(ags/hypr): restore automatic workspaces

--- ags/.config/ags/modules/hyprland.js
+++ ags/.config/ags/modules/hyprland.js
@@ -10,13 +10,23 @@ const dispatch = (ws) => hyprland.messageAsync(`dispatch workspace ${ws}`);
 const Workspaces = Widget.EventBox({
   child: Widget.Box({
     className: "box",
-    children: Array.from({ length: 4 }, (_, i) => i + 1).map((i) =>
+    children: Array.from({ length: 10 }, (_, i) => i + 1).map((i) =>
       Widget.Button({
         attribute: i,
         label: `${i}`,
         onClicked: () => dispatch(i),
       }),
     ),
+
+    // remove this setup hook if you want fixed number of buttons
+    setup: (self) =>
+      self.hook(hyprland, () =>
+        self.children.forEach((btn) => {
+          btn.visible = hyprland.workspaces.some(
+            (ws) => ws.id === btn.attribute,
+          );
+        }),
+      ),
   }),
 });
 
