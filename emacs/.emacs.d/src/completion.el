f10b6e53679901831d992536d35c0c37f51a8e90
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2024-06-06 08:17:31 +0200

    chore(emacs/completion): comment alignment

--- emacs/.emacs.d/src/completion.el
+++ emacs/.emacs.d/src/completion.el
@@ -5,8 +5,8 @@
 
 (require 'vertico)
 
-(setq vertico-cycle t)	; Enable cycling when using vertico-next & vertico-previous
-(vertico-mode)		; Enable vertico
+(setq vertico-cycle t) ; Enable cycling when using vertico-next & vertico-previous
+(vertico-mode)         ; Enable vertico
 
 (require 'marginalia)
 (marginalia-mode)
@@ -15,14 +15,14 @@
 (setq completion-styles '(orderless partial-completion basic))
 
 (require 'corfu)
-(setq corfu-cycle t)		; Cycle corfu candidates
-(setq corfu-auto t)		; Trigger corfu automatically
-(setq corfu-auto-prefix 2)	; Required prefixes for autocompletion
-(setq corfu-auto-delay 0)	; Delay before triggering autocompletion
-(global-corfu-mode)		; Enable corfu globally
+(setq corfu-cycle t)	   ; Cycle corfu candidates
+(setq corfu-auto t)        ; Trigger corfu automatically
+(setq corfu-auto-prefix 2) ; Required prefixes for autocompletion
+(setq corfu-auto-delay 0)  ; Delay before triggering autocompletion
+(global-corfu-mode)        ; Enable corfu globally
 
 (require 'kind-icon)
-(setq kind-icon-default-face 'corfu-default)				; Use corfu colors for icons
+(setq kind-icon-default-face 'corfu-default)                        ; Use corfu colors for icons
 (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter)	; Set kind-icon as default margin formatter
 
 (require 'which-key)
