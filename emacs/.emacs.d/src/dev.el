5d76ab7d7ae8e590c6a64cc97283f7e3ae8a8d5a
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2025-01-18 07:57:11 +0100

    feat(emacs/dev): flycheck in eldoc buffers

--- emacs/.emacs.d/src/dev.el
+++ emacs/.emacs.d/src/dev.el
@@ -41,6 +41,35 @@
 (setq lsp-inlay-hint-enable t)
 
 
+(require 'flycheck)
+(defun flycheck-eldoc (callback &rest _ignored)
+  "Print flycheck messages at point by calling CALLBACK."
+  (when-let ((flycheck-errors (and flycheck-mode (flycheck-overlay-errors-at (point)))))
+    (mapc
+     (lambda (err)
+       (funcall callback
+                (format "%s: %s"
+                        (let ((level (flycheck-error-level err)))
+                          (pcase level
+                            ('info (propertize "I" 'face 'flycheck-error-list-info))
+                            ('error (propertize "E" 'face 'flycheck-error-list-error))
+                            ('warning (propertize "W" 'face 'flycheck-error-list-warning))
+                            (_ level)))
+                        (flycheck-error-message err))
+                :thing (or (flycheck-error-id err)
+                           (flycheck-error-group err))
+                :face 'font-lock-doc-face))
+     flycheck-errors)))
+
+(defun flycheck-prefer-eldoc ()
+  "Add flycheck-eldoc to eldoc doc functions and disabled default flycheck."
+  (add-hook 'eldoc-documentation-functions #'flycheck-eldoc nil t)
+  (setq eldoc-documentation-strategy 'eldoc-documentation-compose-eagerly)
+  (setq flycheck-display-errors-function nil)
+  (setq flycheck-help-echo-function nil))
+
+(add-hook 'flycheck-mode-hook 'flycheck-prefer-eldoc)
+
 (add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-ts-mode))
 (add-hook 'rust-ts-mode-hook 'lsp)
 
