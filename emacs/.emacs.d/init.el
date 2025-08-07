a864ef4e40c702089480dfbde6efb26465f04f90
lindtvedtsebastian <lindtvedt.sebastian@gmail.com>
2025-08-07 11:08:20 +0200

    feat(emacs/lsp): add lsp-sourcekit package

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -57,6 +57,7 @@
     flycheck             ; Linting
     colorful-mode        ; Colorize color names
     lsp-pyright          ; Python LSP client
+    lsp-sourcekit        ; Swift LSP client
     citar                ; Citation completion frontend
     web-mode))           ; Major mode for template based files
 
